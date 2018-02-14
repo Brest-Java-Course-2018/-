7ccea9345eb4602c278712d879fe65dd7649e667
IamCyrek <maychenko1998@gmail.com>
2018-02-15 01:11:20 +0300

    Connect to DB

--- mainapp/mainpart/src/main/java/com/epam/brest/course/App.java
+++ mainapp/mainpart/src/main/java/com/epam/brest/course/App.java
@@ -6,6 +6,8 @@ package com.epam.brest.course;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        DBUtils dbUtils = new DBUtils();
     }
 
     /**
