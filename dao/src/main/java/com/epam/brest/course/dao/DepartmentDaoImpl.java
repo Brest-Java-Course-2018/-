37ac70178e4acfcf05241b8b95e3b898d7d95671
IamCyrek <maychenko1998@gmail.com>
2018-03-06 09:47:12 +0300

    DepartmentDao finished

--- dao/src/main/java/com/epam/brest/course/dao/DepartmentDaoImpl.java
+++ dao/src/main/java/com/epam/brest/course/dao/DepartmentDaoImpl.java
@@ -1,8 +1,8 @@
 package com.epam.brest.course.dao;
 
 import com.epam.brest.course.model.Department;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -11,7 +11,6 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
-import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -21,106 +20,123 @@ import java.util.List;
  */
 public class DepartmentDaoImpl implements DepartmentDao {
 
-    /**
-     * Class JdbcTemplate from spring JDBC.
-     */
-    private JdbcTemplate jdbcTemplate;
+    public static final String DEPARTMENT_ID = "departmentId";
+    public static final String DEPARTMENT_NAME = "departmentName";
+    public static final String DESCRIPTION = "description";
 
     /**
      * Class NamedParameterJdbcTemplate from spring JDBC.
      */
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-    /**
-     * SQL request for get department from database.
-     */
-    private static final String GET_DEPARTMENTS_SQL =
-            "SELECT departmentId, departmentName, description FROM department;";
+    @Value("${department.select}")
+    private String departmentSelect;
+    @Value("${department.selectById}")
+    private String departmentSelectByID;
+    @Value("${department.checkDepartment}")
+    private String departmentcheckDepartment;
+    @Value("${department.insert}")
+    private String departmentInsert;
+    @Value("${department.update}")
+    private String departmentUpdate;
+    @Value("${department.delete}")
+    private String departmentDelete;
 
     /**
-     * SQL request for get departments from database.
-     */
-    private static final String GET_DEPARTMENTS_BY_ID_SQL =
-            "SELECT departmentId, departmentName, description "
-                    + " FROM department WHERE departmentId = :departmentId;";
-
-    /**
-     * SQL request for add department in database.
-     */
-    private static final String ADD_DEPARTMENT_SQL =
-            "INSERT INTO department(departmentName, description) "
-                    + "VALUES (:departmentName, :description);";
-
-    /**
-     * SQL request for update department in database.
-     */
-    private static final String UPDATE_DEPARTMENT_SQL =
-            "UPDATE department SET departmentName = :departmentName, "
-                    + "description = :description WHERE departmentId = :departmentId;";
-
-    /**
-     * SQL request for delete department from database.
-     */
-    private static final String DELETE_DEPARTMENT_BY_ID_SQL =
-            "DELETE FROM department WHERE departmentId = :departmentId;";
-
-    /**
-     * Constructor for class DepartmentDaoImpl.
+     * Set namedParameterJdbcTemplate
      *
-     * @param dataSource from spring JDBC
+     * @param namedParameterJdbcTemplate new namedParameterJdbcTemplate
      */
-    public DepartmentDaoImpl(final DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.namedParameterJdbcTemplate =
-                new NamedParameterJdbcTemplate(dataSource);
+    public void setNamedParameterJdbcTemplate(
+            NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
     @Override
     public List<Department> getDepartments() {
         List<Department> departments =
-                jdbcTemplate.query(GET_DEPARTMENTS_SQL,
-                        new DepartmentRowMapper());
+                namedParameterJdbcTemplate.getJdbcOperations().query(
+                        departmentSelect, new DepartmentRowMapper());
         return departments;
 
     }
 
-    @Override
+    /*@Override
     public Department getDepartmentById(final Integer departmentId) {
         SqlParameterSource namedParametrs =
-                new MapSqlParameterSource("departmentId", departmentId);
+                new MapSqlParameterSource(DEPARTMENT_ID, departmentId);
         try {
             Department department = namedParameterJdbcTemplate.queryForObject(
-                    GET_DEPARTMENTS_BY_ID_SQL,
+                    departmentSelectByID,
                     namedParametrs,
                     new DepartmentRowMapper());
             return department;
         } catch (EmptyResultDataAccessException e) {
             return null;
         }
+    }*/
+
+    @Override
+    public Department getDepartmentById(Integer departmentId) {
+        SqlParameterSource namedParametrs =
+                new MapSqlParameterSource(DEPARTMENT_ID, departmentId);
+        Department department = namedParameterJdbcTemplate.queryForObject(
+                departmentSelectByID, namedParametrs,
+                BeanPropertyRowMapper.newInstance(Department.class));
+
+        return department;
     }
 
     @Override
     public Department addDepartment(final Department department) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
+        /*KeyHolder keyHolder = new GeneratedKeyHolder();
         namedParameterJdbcTemplate.update(
                 ADD_DEPARTMENT_SQL,
                 new BeanPropertySqlParameterSource(department),
                 keyHolder);
-        return getDepartmentById(keyHolder.getKey().intValue());
+        department.setDepartmentId(keyHolder.getKey().intValue());
+        return department;*/
+
+        MapSqlParameterSource namedParametrs = new MapSqlParameterSource(
+                DEPARTMENT_NAME, department.getDepartmentName());
+
+        Integer result = namedParameterJdbcTemplate.queryForObject(
+                departmentcheckDepartment, namedParametrs, Integer.class);
+
+        if (result == 0) {
+            namedParametrs = new MapSqlParameterSource();
+            namedParametrs.addValue(
+                    DEPARTMENT_NAME, department.getDepartmentName());
+            namedParametrs.addValue(DESCRIPTION, department.getDescription());
+
+            KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+
+            namedParameterJdbcTemplate.update(
+                    departmentInsert, namedParametrs, generatedKeyHolder);
+            department.setDepartmentId(generatedKeyHolder.getKey().intValue());
+        } else {
+            throw new IllegalArgumentException(
+                    "Department with the same name already exists");
+        }
+
+        return department;
     }
 
     @Override
     public void updateDepartment(final Department department) {
-        namedParameterJdbcTemplate.update(
+        /*namedParameterJdbcTemplate.update(
                 UPDATE_DEPARTMENT_SQL,
-                new BeanPropertySqlParameterSource(department));
+                new BeanPropertySqlParameterSource(department));*/
+
+        SqlParameterSource namedParameter =
+                new BeanPropertySqlParameterSource(department);
+        namedParameterJdbcTemplate.update(departmentUpdate, namedParameter);
     }
 
     @Override
-    public void deleteDepartmentById(final Integer id) {
-        namedParameterJdbcTemplate.update(
-                DELETE_DEPARTMENT_BY_ID_SQL,
-                new MapSqlParameterSource().addValue("departmentId", id));
+    public void deleteDepartmentById(final Integer departmentId) {
+        namedParameterJdbcTemplate.getJdbcOperations().update(
+                departmentDelete, departmentId);
     }
 
     /**
@@ -133,9 +149,9 @@ public class DepartmentDaoImpl implements DepartmentDao {
         public Department mapRow(final ResultSet resultSet, final int i)
                 throws SQLException {
             Department department = new Department();
-            department.setDepartmentId(resultSet.getInt(1));
-            department.setDepartmentName(resultSet.getString(2));
-            department.setDescription(resultSet.getString(3));
+            department.setDepartmentId(resultSet.getInt(DEPARTMENT_ID));
+            department.setDepartmentName(resultSet.getString(DEPARTMENT_NAME));
+            department.setDescription(resultSet.getString(DESCRIPTION));
             return department;
         }
     }
--- dao/src/test/java/com/epam/brest/course/dao/DepartmentDaoImplTest.java
+++ dao/src/test/java/com/epam/brest/course/dao/DepartmentDaoImplTest.java
@@ -2,17 +2,23 @@ package com.epam.brest.course.dao;
 
 import com.epam.brest.course.model.Department;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:test-db-spring.xml",
-        "classpath:test-dao.xml"})
+        "classpath:test-dao.xml", "classpath:dao.xml"})
+@Rollback
+@Transactional
 public class DepartmentDaoImplTest {
 
     @Autowired
@@ -37,7 +43,7 @@ public class DepartmentDaoImplTest {
 
     @Test
     public void addDepartment() {
-        final String DEP_NAME = "Management";
+        /*final String DEP_NAME = "Management";
         final String DEP_DESCR = "Management Department";
 
         Department dep = new Department();
@@ -48,12 +54,45 @@ public class DepartmentDaoImplTest {
 
         Assert.assertNotNull(department);
         Assert.assertTrue(department.getDepartmentName().equals(DEP_NAME));
-        Assert.assertTrue(department.getDescription().equals(DEP_DESCR));
+        Assert.assertTrue(department.getDescription().equals(DEP_DESCR));*/
+
+        List<Department> departments = departmentDao.getDepartments();
+        int sizeBefore = departments.size();
+        Department department = new Department("Education and Training",
+                "Department Education and Training");
+        Department newDepartment = departmentDao.addDepartment(department);
+        Assert.assertNotNull(newDepartment.getDepartmentId());
+        Assert.assertTrue(newDepartment.getDepartmentName().equals(department.getDepartmentName()));
+        Assert.assertTrue(newDepartment.getDescription().equals(department.getDescription()));
+        Assert.assertTrue(sizeBefore + 1 == departmentDao.getDepartments().size());
+        Assert.assertTrue(department.equals(newDepartment));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void addSameDepartment() {
+        Department department = new Department("Education and Training",
+                "Department of Education and Training");
+        departmentDao.addDepartment(department);
+        departmentDao.addDepartment(department);
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void addSameDepartmentWithRule() {
+        Department department = new Department("Education and Training",
+                "Department of Education and Training");
+        departmentDao.addDepartment(department);
+
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Department with the same name already exists");
+        departmentDao.addDepartment(department);
     }
 
     @Test
     public void updateDepartment() {
-        Department dep = new Department();
+        /*Department dep = new Department();
         dep.setDepartmentId(2);
         dep.setDepartmentName("Supp");
         dep.setDescription("Support Team");
@@ -62,14 +101,35 @@ public class DepartmentDaoImplTest {
         Department department = departmentDao.getDepartmentById(2);
 
         Assert.assertNotNull(department);
-        Assert.assertEquals(dep, department);
+        Assert.assertEquals(dep, department);*/
+
+        Department department =
+                new Department("Education", "Department of Education");
+        Department newDepartment = departmentDao.addDepartment(department);
+        newDepartment.setDepartmentName("NEW Education");
+        newDepartment.setDescription("NEW Department of Education");
+
+        departmentDao.updateDepartment(newDepartment);
+        Department updateDepartment = departmentDao.getDepartmentById(newDepartment.getDepartmentId());
+        Assert.assertTrue(newDepartment.getDepartmentId().equals(updateDepartment.getDepartmentId()));
+        Assert.assertTrue(newDepartment.getDepartmentName().equals(updateDepartment.getDepartmentName()));
+        Assert.assertTrue(newDepartment.getDescription().equals(updateDepartment.getDescription()));
+
     }
 
 
     @Test
     public void deleteDepartmentById() {
-        departmentDao.deleteDepartmentById(3);
+        /*departmentDao.deleteDepartmentById(3);
         Department department = departmentDao.getDepartmentById(3);
-        Assert.assertTrue(department == null);
+        Assert.assertTrue(department == null);*/
+
+        Department department =
+                new Department("Education", "Department of Education");
+        department = departmentDao.addDepartment(department);
+        List<Department> departments = departmentDao.getDepartments();
+        int sizeBefore = departments.size();
+        departmentDao.deleteDepartmentById(department.getDepartmentId());
+        Assert.assertTrue(sizeBefore - 1 == departmentDao.getDepartments().size());
     }
 }
