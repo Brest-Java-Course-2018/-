9d1003984c605bbd85671668918f2365424dede0
IamCyrek <maychenko1998@gmail.com>
2018-03-29 06:51:24 +0300

    Fixed bugs in error handler.

--- web-app/src/main/java/com/epam/brest/course/web_app/controllers/ErrorController.java
+++ web-app/src/main/java/com/epam/brest/course/web_app/controllers/ErrorController.java
@@ -1,31 +1,71 @@
 package com.epam.brest.course.web_app.controllers;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
- * Controller for error page.
+ * Rest controller for error page.
  */
-@ControllerAdvice
-public class ErrorController extends RuntimeException {
+@RestController
+public class ErrorController {
 
-    private static final Logger LOGGER = LogManager.getLogger();
+    @RequestMapping(path = "/error")
+    public ModelAndView handle(HttpServletRequest request) {
+        ModelAndView errorPage = new ModelAndView("error");
+        String errorMsg = "Http Error Code " +
+                request.getAttribute("javax.servlet.error.status_code") + ": ";
 
-    /**
-     * Exception handler.
-     *
-     * @param exc   Exception.
-     * @param model model.
-     * @return template name.
-     */
-    @ExceptionHandler(Exception.class)
-    public final String exceptionHandler(final Exception exc,
-                                         final Model model) {
-        LOGGER.debug("exceptionHandler({})", exc.getMessage());
-        model.addAttribute("text", exc.getLocalizedMessage());
-        return "error";
+        switch (getErrorCode(request)) {
+            case 500:
+                errorMsg += "Internal Server Error";
+                break;
+            case 501:
+                errorMsg += "Not Implemented";
+                break;
+            case 502:
+                errorMsg += "Bad Gateway";
+                break;
+            case 503:
+                errorMsg += "Service Unavailable";
+                break;
+            case 504:
+                errorMsg += "Gateway Timeout";
+                break;
+            case 505:
+                errorMsg += "HTTP Version Not Supported";
+                break;
+            case 506:
+                errorMsg += "Variant Also Negotiates";
+                break;
+            case 507:
+                errorMsg += "Insufficient Storage";
+                break;
+            case 508:
+                errorMsg += "Loop Detected";
+                break;
+            case 509:
+                errorMsg += "Bandwidth Limit Exceeded";
+                break;
+            case 510:
+                errorMsg += "Not Extended";
+                break;
+            case 511:
+                errorMsg += "Network Authentication Required";
+                break;
+            default:
+                errorMsg += request
+                        .getAttribute("javax.servlet.error.message");
+        }
+
+        errorPage.addObject("text", errorMsg);
+        return errorPage;
+    }
+
+    private int getErrorCode(HttpServletRequest request) {
+        return (int) request
+                .getAttribute("javax.servlet.error.status_code");
     }
 }
